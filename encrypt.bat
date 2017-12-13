61f1f5cffa46983de0362c171fe305a1aee6caec
ndxbxrme <lewis_the_cat@hotmail.com>
2017-12-13 08:14:52 +0000

    key stuff

--- encrypt.bat
+++ encrypt.bat
@@ -1,4 +1,4 @@
 call openssl aes-256-cbc -e -in env.sh -out env-enc.sh -k %ENCRYPTION_KEY%
 call openssl aes-256-cbc -e -in cert.pem -out cert-enc.pem -k %ENCRYPTION_KEY%
 call openssl aes-256-cbc -e -in key.pem -out key-enc.pem -k %ENCRYPTION_KEY%
-call openssl aes-256-cbc -e -in certs/vitalspacelive-enc.p12 -out certs/vitalspacelive.p12 -k %ENCRYPTION_KEY%
+call openssl aes-256-cbc -e -in certs/vitalspacelive.p12 -out certs/vitalspacelive-enc.p12 -k %ENCRYPTION_KEY%
